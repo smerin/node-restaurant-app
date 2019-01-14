35d78e2f388df27d1748814efbbf96eef87399aa
George Smerin <gsmerin@gmail.com>
2019-01-14 07:37:03 +0000

    Add store model and edit store form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
 exports.homePage = (req, res) => {
   res.render("index");
 };
+
+exports.addStore = (req, res) => {
+  res.render("editStore", {
+    title: "Add store"
+  });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
