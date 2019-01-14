8501333f660f40b693558fb2ee70382fb3a4b391
George Smerin <george.smerin@flippercommunity.com>
2019-01-14 17:31:25 +0000

    Add save store functionality. Add flash message and dynamic redirect

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
 exports.homePage = (req, res) => {
   res.render("index");
 };
@@ -8,6 +11,11 @@ exports.addStore = (req, res) => {
   });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
 };
