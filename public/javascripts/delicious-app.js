c1cc10a50052db9234481d65bf8ba816dce4fd63
George Smerin <george.smerin@flippercommunity.com>
2019-01-15 17:30:06 +0000

    Add location fields

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,7 @@
-import '../sass/style.scss';
+import "../sass/style.scss";
 
-import { $, $$ } from './modules/bling';
+import { $, $$ } from "./modules/bling";
+
+import autoComplete from "./modules/autocomplete";
+
+autoComplete($("#address"), $("#lat"), $("#lng"));
