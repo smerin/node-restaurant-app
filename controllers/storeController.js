c172001cfc09d89b5c9b60e6ecf9fec6b75cb3a3
George Smerin <gsmerin@gmail.com>
2019-01-26 12:36:46 +0000

    Add store single page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -88,3 +88,10 @@ exports.updateStore = async (req, res) => {
   );
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  // res.json(store);
+  res.render("store", { title: store.name, store });
+};
