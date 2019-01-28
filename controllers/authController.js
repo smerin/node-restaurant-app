3670d5e08895706f21636348ef5ac44875f4cfd0
George Smerin <george.smerin@flippercommunity.com>
2019-01-28 17:54:01 +0000

    Add account page

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,3 +21,11 @@ exports.isLoggedIn = (req, res, next) => {
   req.flash("error", "You must be logged in to do that");
   res.redirect("/login");
 };
+
+exports.forgot = async (req, res) => {
+  // 1. See if that user exists
+  const user = await user.findOne({ email: req.body.email });
+  // 2. Set reset tokens and expiry on their account
+  // 3. Send them an email with the token
+  // 4. Redirect to login page after email token has been sent
+};
--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,9 @@ router.post(
 
 router.get("/logout", authController.logout);
 
+router.get("/account", authController.isLoggedIn, userController.account);
+router.post("/account", catchErrors(userController.updateAccount));
+
+router.post("/account/forgot", catchErrors(authController.forgot));
+
 module.exports = router;
